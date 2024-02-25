bc440777e1e2e64096f1179b68f33e45964ac47a
dragan002 <draganvujic29@gmail.com>
2024-02-25 11:24:53 +0100

    Login Logic  - separating Login admin redirection from users

--- app/controllers/loginController.php
+++ app/controllers/loginController.php
@@ -3,16 +3,26 @@
 require_once('../../app/initialize.php');
 include(SHARED_PATH . '/login_header.php');
 
+
+if(isset($_GET['email'])) {
+    $email = $_GET['email'];
+    $user = find_user_by_email($email);
+}   
+
+
+
+
 $errors = [];
 
-if (is_post_request()) {
+if (is_get_request()) {
     try {
-        $logger_data = get_logger_from_form();
+        $logger_data = get_logger_from_forms();
         
         if (!$logger_data) {
             $errors[] = "Form submission error: No data received";
         } 
         
+        if($logger_data['email']) {}
         $email = $logger_data['email'];
         $password = $logger_data['password'];
     
@@ -21,8 +31,11 @@ if (is_post_request()) {
         if (!$match) {
             return false;
         } 
-
-        header('Location: ../../app/view/admin_homepage.php');
+        
+        if( $user['role'] == 'admin') {
+            header("Location: ..private/view/admin_homepage.php");
+        };
+        
         exit();
         
     } catch (Exception $e) {
--- app/models/userModel.php
+++ app/models/userModel.php
@@ -1,5 +1,23 @@
 <?php
 
+function find_all_users() {
+    global $db;
+
+    try {
+        $sql = "SELECT * FROM users";
+        $stmt = $db->prepare($sql);
+        $stmt->execute();
+    
+        $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    
+        confirmResultSet($result);
+
+        return $result;
+    } catch (PDOException $e) {
+        die("Error in query: " . $sql . " - " . $e->getMessage());
+    }
+}
+
 function login($email, $password) {
     global $db;
 
@@ -28,7 +46,6 @@ function login($email, $password) {
         $stmt = null;
     }
 }
-
 function createUser($user) {
     global $db;
 
@@ -53,10 +70,6 @@ function createUser($user) {
         die("Failed to insert data into the database: " . $e->getMessage());
     }
 }
-
-
-
-
 function get_logger_from_form() {
     if(!isset($_POST['email']) && !isset($_POST['password'])) {
         return false;
@@ -67,4 +80,34 @@ function get_logger_from_form() {
     'password' => $_POST['password']
     ];
     return $logger;
+}
+
+function get_logger_from_forms() {
+    if(!isset($_GET['email']) && !isset($_GET['password'])) {
+        return false;
+    } 
+
+    $logger = [
+    'email' => filter_var($_GET['email'], FILTER_SANITIZE_EMAIL),
+    'password' => $_GET['password']
+    ];
+    return $logger;
+}
+
+function find_user_by_email($email) {
+    global $db;
+
+    try {
+        $sql = "SELECT * FROM users WHERE email = :email LIMIT 1";
+    
+        $stmt = $db->prepare($sql);
+        $stmt->bindParam(':email', $email, PDO::PARAM_STR);
+        $stmt->execute();
+    
+        $result = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        return $result;
+    }  catch (PDOException $e) {
+        die("Failed to retrieve data from database: " . $e->getMessage());
+    }
 }
