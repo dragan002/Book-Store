86af68705737d317112bcb9a31fe27b57b85d20c
dragan002 <draganvujic29@gmail.com>
2024-03-11 19:39:15 +0100

    Working on Login OOP

--- app/controllers/userControllers/loginController.php
+++ app/controllers/userControllers/loginController.php
@@ -3,9 +3,11 @@
 require_once('../../app/initialize.php');
 include(SHARED_PATH . '/login_header.php');
 
+$userInstance = new User();
+
 if (isset($_POST['email'])) {
     $email = $_POST['email'];
-    $user = find_user_by_email($email);
+    $user = $userInstance->findUserByEmail($email);
 }
 
 $errors = [];
@@ -13,7 +15,7 @@ $errors = [];
 // Negacija prvo
 if (is_post_request()) {
     try {
-        $logger_data = get_logger_from_form();
+        $logger_data = $userInstance->getLoggerFromForm();
 
         if (!$logger_data) {
             $errors[] = "Form submission error: No data received";
@@ -21,13 +23,13 @@ if (is_post_request()) {
             $email = $logger_data['email'];
             $password = $logger_data['password'];
 
-            $match = login($email, $password);
+            $match = $userInstance->login($email, $password);
 
             if (!$match) {
                 throw new Exception("Incorrect email or password");
             }
             
-            handleSuccessfulLogin($user);
+            $userInstance->handleSuccessfulLogin($user);
         }
     } catch (Exception $e) {
         $errors[] = $e->getMessage();
