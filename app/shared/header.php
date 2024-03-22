862f2fda701bc1b6107385585ecbd1d5ea40c12a
dragan002 <draganvujic29@gmail.com>
2024-03-22 20:44:59 +0100

    SCSS

--- app/shared/header.php
+++ app/shared/header.php
@@ -9,6 +9,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title><?php echo $pageTitle ?></title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="\public\resource\scss\style.css">
 </head>
 
 <body>
