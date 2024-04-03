f8d33577b39dbadc3910257ed2e2eec9122e5f13
dragan002 <draganvujic29@gmail.com>
2024-04-03 15:51:00 +0200

    Cleaning code

--- app/shared/header.php
+++ app/shared/header.php
@@ -1,5 +1,9 @@
-<?php 
-    session_start();
+<?php session_start();
+$bookInstance = new App\models\classes\Book\Book();
+
+$books = $bookInstance->findAllBooks();
+
+$categories = $bookInstance->findAllCategories();
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -9,36 +13,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title><?php echo $pageTitle ?></title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-<<<<<<< Updated upstream
-    <link rel="stylesheet" href="\public\resource\scss\style.css">
-=======
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <script src="/public/design/js/javascript.js"></script>
-
-    <link rel="stylesheet" href="\public\design\style\style.css">
->>>>>>> Stashed changes
+    <link rel="stylesheet" href="..\..\public\design\style\style.css">
 </head>
 
 <body>
 
-<nav class="header">
-    <div class="header__container">        
-       
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark shadow">
+    <a class="navbar-brand" href="#">
+        <img class="navbar__logo" src="../../public/image/logo1.png" alt="">
+    </a>
 
-<<<<<<< Updated upstream
-        <?php if(isset($_SESSION['username'])) { ?>
-    <div class="alert alert-success text-center" role="alert">
-        Hello, <?php echo $_SESSION['username']; ?>! Welcome to BookStore.
-    </div>
-    <?php } ?> 
-
-        <div class="background-secondary" id="navbarNav">
-            <ul class="grid h6">
-                <a class="navbar-brand col-span-2" href="#">Your Logo</a>
-
-                <li class="background-secondary col-span-2">
-                    <a class="" href="../../public/index.php"></a>
-=======
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
             aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -52,66 +37,42 @@
         <?php } ?>
         <!-- End Welcome Message -->
         <ul class="navbar-nav mr-auto">
+            <!-- Home link -->
             <li class="nav-item active">
                 <a class="nav-link" href="../../public/index.php">Home</a>
             </li>
-
-
-        </ul>
-        <ul class="navbar-nav">
-            <?php if(isset($_SESSION['username'])) { ?>
-            <li class="nav-item">
-                <a class="nav-link" href="../../public/pages/cartItems.php">
-                    <i class="fas fa-shopping-cart"></i> Cart
+            <!-- Categories dropdown -->
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Categories
                 </a>
             </li>
-            <?php } ?>
-            <?php if (!isset($_SESSION['username'])) { ?>
+        </ul>
+
+        <ul class="navbar-nav">
+            <?php if(isset($_SESSION['username'])) { ?>
                 <li class="nav-item">
-                    <a class="nav-link btn btn-primary text-white mr-2" href="../../public/pages/login.php">Login</a>
->>>>>>> Stashed changes
-                </li>
-
-                <li class="nav-item col-span-1">
-                    <a class="nav-link" href="../../public/pages/about.php">About Us</a>
-                </li>
-
-                <li class="nav-item col-span-1">
-                    <a class="nav-link" href="../../public/pages/services.php">Services</a>
-                </li>
-
-                <li class="nav-item col-span-1">
-                    <a class="nav-link" href="../../public/pages/contact.php">Contact</a>
-                </li>
-
-                <li class="c-button col-span-1">
                     <a class="nav-link" href="../../public/pages/cartItems.php">
                         <i class="fas fa-shopping-cart"></i> Cart
                     </a>
                 </li>
-
-                <!-- Login and Register Buttons -->  
-                <?php if(!isset($_SESSION['username'])) { ?>               
-                    <li class="c-button col-span-1">
-                        <a class="nav-link btn btn-outline-primary" href="../../public/pages/login.php">Login</a>
-                    </li>
-                <?php } ?>
-
-                <?php if(!isset($_SESSION['username'])) { ?>               
-                    <li class="nav-item">
-                        <a class="nav-link btn btn-success" href="../../public/pages/registration.php">Register</a>
-                    </li>
-                <?php } ?>
-                
-                <?php if(isset($_SESSION['username'])) { ?>
-                    <li class="nav-item col-span-1">
-                        <a class="nav-link" href="../../app/controllers/userControllers/logOutController.php">Logout</a>
-                    </li>
-                <?php } ?>
-            </ul>
-        </div>
+            <?php } ?>
+            <?php if (!isset($_SESSION['username'])) { ?>
+                <li class="nav-item">
+                    <a class="nav-link btn btn-primary text-white mr-2" href="../../public/pages/login.php">Login</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link btn btn-success text-white" href="../../public/pages/registration.php">Register</a>
+                </li>
+            <?php } else { ?>
+                <li class="nav-item">
+                    <a class="nav-link" href="../../app/controllers/userControllers/logOutController.php">Logout</a>
+                </li>
+            <?php } ?>
+        </ul>
     </div>
 </nav>
 
+</body>
 
-<!-- The rest of your HTML content goes here -->
+</html>
